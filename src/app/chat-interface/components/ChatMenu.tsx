b9737662e4fbd32cc742047acca208ef8591e894
rayhan <rayhananthaakbar@gmail.com>
2025-05-22 19:49:06 +0700

    chore(style): adjust menu size

--- src/app/chat-interface/components/ChatMenu.tsx
+++ src/app/chat-interface/components/ChatMenu.tsx
@@ -3,7 +3,7 @@ import ChatNav from "./ChatNav";
 
 export default function ChatMenu({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <div id="chat-menu" className="flex w-full">
+        <div id="chat-menu" className="flex">
             <ChatNav />
             {children}
         </div>
